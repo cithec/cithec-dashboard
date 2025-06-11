7931fa41e96df71167d3a964da00a40469271c01
cithec <centro.cithec@gmail.com>
2025-06-11 18:16:55 -0400

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-
-function showSection(sectionId) {
-  const sections = document.querySelectorAll('main > section');
-  sections.forEach(section => section.classList.add('hidden'));
-  document.getElementById(sectionId).classList.remove('hidden');
-}
-window.onload = () => showSection('dashboard');
