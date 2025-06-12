9d0f31fe8f720151d6a17357002bb826e4058cc4
cithec <centro.cithec@gmail.com>
2025-06-12 18:06:19 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-function showSection(sectionId) {
-  const sections = ['dashboard', 'citas', 'pacientes', 'medicos', 'config'];
-  sections.forEach(id => {
-    document.getElementById(id).classList.add('hidden');
-  });
-  document.getElementById(sectionId).classList.remove('hidden');
-}
